4d9f418b4039419fcde0e6d8b9db85466177fdc0
AleFuentes982 <jano231982@gmail.com>
2021-10-09 23:45:44 -0300

    first commit

---
+++ eliminalista.py
@@ -0,0 +1,7 @@
+list1 = [0,1,2,3,4]
+print("los elementos de la lista son: ", list1)
+print("el largo de la lista original es: ", len(list1))
+del list1[1]
+print("\n")
+
+print("los elementos de la lista modificada son: ", list1, "y su largo es: ", len(list1))
